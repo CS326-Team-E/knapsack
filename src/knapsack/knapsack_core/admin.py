82f12d46ce03afc9af1a53ba21736a646c653287
Conan <vagrant@vagrant.vm>
2018-10-18 18:16:55 +0000

    question model and admin

--- src/knapsack/knapsack_core/admin.py
+++ src/knapsack/knapsack_core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from knapsack_core.models import User, Question
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Question)
--- src/knapsack/knapsack_core/models.py
+++ src/knapsack/knapsack_core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+  username = models.ForeignKey('User', on_delete = models.SET_NULL, null=True)
+  question = models.CharField(max_length=200)
+  answer = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.question
+
+  def get_absolute_url(self):
+    return reverse('question-detail', args=[str(self.id)])
