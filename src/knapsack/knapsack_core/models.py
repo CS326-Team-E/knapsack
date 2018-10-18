276307aeb4f2ec99bb61cdb5aaa06488e9bff67e
JacobGo <jacobmgoldman@icloud.com>
2018-10-18 14:13:24 -0400

    Added user, knapsack, tool

--- src/knapsack/knapsack_core/models.py
+++ src/knapsack/knapsack_core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+
+    identifier = models.CharField(max_length=40, unique=True)
+    user_knapsack = models.ForeignKey('Knapsack', on_delete=models.SET_NULL, null=True)
+    USERNAME_FIELD='identifier'
+
+
+class Knapsack(models.Model):
+
+    owner = models.ForeignKey('User', on_delete=models.CASCADE, null=False)
+    tools = models.ManyToManyField('Tool')
+    theme = 'DEFAULT' # TODO: implement themes
+
+class Tool(models.Model):
+
+    identifier = models.CharField(max_length=40, unique=True)
+    path = models.CharField(max_length=40)
+    description = models.TextField()
