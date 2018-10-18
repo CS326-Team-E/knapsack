b001e6989ee8a0dbb28b176a713d595ae4b66592
Conan <vagrant@vagrant.vm>
2018-10-18 17:34:02 +0000

    skeleton website

---
+++ src/knapsack/knapsack_core/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KnapsackCoreConfig(AppConfig):
+    name = 'knapsack_core'
---
+++ src/knapsack/knapsack_core/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from knapsack_core import views
+
+urlpatterns = [
+
+]
