4921b247e5b8a70642148098beb10cffdb6c4f99
Conan <vagrant@vagrant.vm>
2018-10-19 20:43:01 +0000

    added __str__ methods

--- src/knapsack/knapsack_core/models.py
+++ src/knapsack/knapsack_core/models.py
@@ -16,11 +16,17 @@ class Knapsack(models.Model):
     tools = models.ManyToManyField('Tool')
     theme = 'DEFAULT' # TODO: implement themes
 
+    def __str__(self):
+      return str(self.owner) + "'s knapsack"
+
 class Tool(models.Model):
 
     identifier = models.CharField(max_length=40, unique=True)
     path = models.CharField(max_length=40)
     description = models.TextField()
+
+    def __str__(self):
+      return self.identifier
 
 class Question(models.Model):
   username = models.ForeignKey('User', on_delete = models.SET_NULL, null=True)
