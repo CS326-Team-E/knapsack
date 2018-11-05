8eccfa928f95eeb3b3e4bd51d5104979bdb98f9f
JacobGo <jacobmgoldman@icloud.com>
2018-11-05 16:07:47 -0500

    Added button states to reflect user status

--- src/knapsack/knapsack_core/views.py
+++ src/knapsack/knapsack_core/views.py
@@ -10,8 +10,14 @@ def request_component(request):
   requests = ToolRequest.objects.all()
   for req in requests:
     req.votes = len(ToolVote.objects.filter(request_id=req.id))
-    req.voted = request.user.id not in [tv.username.id for tv in ToolVote.objects.all()]
+    req.voted = request.user.id in [tv.username.id for tv in ToolVote.objects.all()]
     req.votable = request.user.is_authenticated and not req.voted
+    req.button = 'disabled'
+    if req.voted:
+      req.button = 'active'
+    elif req.votable:
+      req.button = ''
+  requests = sorted( list(requests), key=lambda r:r.votes, reverse=True)
   return render(request, 'request_component.html', context={
     'requests': requests
   })
