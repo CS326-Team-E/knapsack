12bf3d7092ead51f1e8ea87f521cfcdfe93c09ad
Jingshu Meng <jingshumeng@1x-nat-vl930-172-30-125-140.wireless.umass.edu>
2018-11-05 16:51:29 -0500

    finished nav on map

--- src/knapsack/knapsack_core/urls.py
+++ src/knapsack/knapsack_core/urls.py
@@ -4,6 +4,6 @@ from knapsack_core import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('map/', views.MapView.as_view(), name='map'),
-    path('request', views.request_component, name='request'),
+    path('request/', views.request_component, name='request'),
     path('library/', views.LibraryView.as_view(), name='library'),
 ]
