2606ba892627035bf752c60107d0cc9d14f75339
GitHubdeWill <hd209458inj@gmail.com>
2018-11-15 13:55:11 -0500

    switch to built-in user

--- src/knapsack/knapsack_core/models.py
+++ src/knapsack/knapsack_core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, User
 
 # Create your models here.
 
