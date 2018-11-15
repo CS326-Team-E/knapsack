09dad6155ec7582794750cbf4237f21527181288
GitHubdeWill <hd209458inj@gmail.com>
2018-11-15 13:42:17 -0500

    added test user bigbro@email.com passwd bigbro

--- src/knapsack/init.py
+++ src/knapsack/init.py
@@ -2,80 +2,99 @@
 from django.contrib.auth.models import User as DjangoUser
 from faker import Faker
 from django.utils import timezone
-import random 
+import random
 
-from knapsack_core.models import User, Question, Knapsack, Tool, ToolRequest, ToolVote
+from knapsack_core.models import User, Question, Knapsack
+from knapsack_core.models import Tool, ToolRequest, ToolVote
 
 fake = Faker()
 
 # Create Users
 users = []
 for i in range(0, 50):
-  a_identifier = fake.safe_email()
-  a_password = fake.password()
-  a_last_login = fake.date_time_this_month(
-    before_now=True, after_now=False, tzinfo=timezone.get_default_timezone()
-  )
-  user = User(identifier=a_identifier, password=a_password, last_login=a_last_login)
-  user.save()
-  users.append(user)
+    a_identifier = fake.safe_email()
+    a_password = fake.password()
+    a_last_login = fake.date_time_this_month(
+        before_now=True,
+        after_now=False,
+        tzinfo=timezone.get_default_timezone()
+    )
+    user = User(identifier=a_identifier,
+                password=a_password, last_login=a_last_login)
+    user.save()
+    users.append(user)
+
+a_identifier = "bigbro@email.com"
+a_password = "bigbro"
+a_last_login = fake.date_time_this_month(
+    before_now=True,
+    after_now=False,
+    tzinfo=timezone.get_default_timezone()
+)
+user = User(identifier=a_identifier,
+            password=a_password, last_login=a_last_login)
+user.save()
+users.append(user)
+
 
 # Create Questions
 questions = []
 for user in users:
-  # Generate a random number of questions for each user
-  random_max = fake.random_int(0, 3)
-  for i in range(0, random_max):
-    a_username = user
-    a_question = fake.text(70)
-    a_answer = fake.text(40)
-    question = Question(username=a_username, question=a_question, answer=a_answer)
-    question.save()
-    questions.append(question)
+    # Generate a random number of questions for each user
+    random_max = fake.random_int(0, 3)
+    for i in range(0, random_max):
+        a_username = user
+        a_question = fake.text(70)
+        a_answer = fake.text(40)
+        question = Question(username=a_username,
+                            question=a_question, answer=a_answer)
+        question.save()
+        questions.append(question)
 
 # Create Tools
 tools = []
 for i in range(0, 10):
-  a_identifier = fake.text(40)
-  a_path = fake.uri_path(deep=3) 
-  a_description = fake.text(200)
-  tool = Tool(identifier=a_identifier, path=a_path, description=a_description)
-  tool.save()
-  tools.append(tool)
+    a_identifier = fake.text(40)
+    a_path = fake.uri_path(deep=3)
+    a_description = fake.text(200)
+    tool = Tool(identifier=a_identifier, path=a_path,
+                description=a_description)
+    tool.save()
+    tools.append(tool)
 
 # Create Knapsacks
 knapsacks = []
 for user in users:
-  knapsack = Knapsack(owner=user)
-  knapsack.save()
-  # Add a random number of tools
-  random_max = fake.random_int(0, len(tools))
-  for i in range(0, random_max):
-    knapsack.tools.add(tools[fake.random_int(0, len(tools)) - 1])
-  knapsacks.append(knapsack)
+    knapsack = Knapsack(owner=user)
+    knapsack.save()
+    # Add a random number of tools
+    random_max = fake.random_int(0, len(tools))
+    for i in range(0, random_max):
+        knapsack.tools.add(tools[fake.random_int(0, len(tools)) - 1])
+    knapsacks.append(knapsack)
 
 for knapsack in knapsacks:
-  user = knapsack.owner
-  user.user_knapsack = knapsack
-  user.save()
+    user = knapsack.owner
+    user.user_knapsack = knapsack
+    user.save()
 
 # Create ToolRequests
 toolrequests = []
-for i in range(0,5):
-  tr = ToolRequest(username=random.choice(users))
-  tr.title = str.title(fake.text(10))
-  tr.request = fake.text(50)
-  tr.save()
-  toolrequests.append(tr)
+for i in range(0, 5):
+    tr = ToolRequest(username=random.choice(users))
+    tr.title = str.title(fake.text(10))
+    tr.request = fake.text(50)
+    tr.save()
+    toolrequests.append(tr)
 
 # Create votes
 for tr in toolrequests:
-  available_users = users.copy()
-  for i in range(0, random.randint(5,25)):
-    user = random.choice(available_users)
-    available_users.remove(user)
-    vote = ToolVote(username=user, request=tr)
-    vote.save()
+    available_users = users.copy()
+    for i in range(0, random.randint(5, 25)):
+        user = random.choice(available_users)
+        available_users.remove(user)
+        vote = ToolVote(username=user, request=tr)
+        vote.save()
 
 username = "admin"
 password = "admin"
--- src/knapsack/knapsack_core/admin.py
+++ src/knapsack/knapsack_core/admin.py
@@ -1,39 +1,45 @@
 from django.contrib import admin
-from knapsack_core.models import User, Question, Knapsack, Tool, ToolRequest, ToolVote
+from knapsack_core.models import User, Question, Knapsack
+from knapsack_core.models import Tool, ToolRequest, ToolVote
 
 # Register your models here.
 
+
 # To be used by UserAdmin
 class QuestionInline(admin.TabularInline):
-	model = Question
-	extra = 0
-	
+    model = Question
+    extra = 0
+
+
 # To be used by UserAdmin
 class KnapsackInline(admin.TabularInline):
-	model = Knapsack
-	extra = 0
-	
+    model = Knapsack
+    extra = 0
+
+
 # Register the Admin class with the User model using the decorator
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-	list_display = ("identifier", "last_login", "user_knapsack")
-	# This allows to display information about the corresponding questions and knapsack of this User
-	inlines = [QuestionInline, KnapsackInline]
-	
+    list_display = ("identifier", "last_login", "user_knapsack")
+    # This allows to display information about the corresponding questions and
+    # knapsack of this User
+    inlines = [QuestionInline, KnapsackInline]
+
+
 # Register the Admin class with the Question model using the decorator
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-	list_display = ("question", "username")
+    list_display = ("question", "username")
+
 
 # Register the other models normally
 admin.site.register(Knapsack)
 admin.site.register(Tool)
 
 
-
 @admin.register(ToolRequest)
 class ToolRequestAdmin(admin.ModelAdmin):
-	list_display = ('username', 'title', 'request', 'get_votes')
+    list_display = ('username', 'title', 'request', 'get_votes')
 
-	def get_votes(self, obj):
-		return len(ToolVote.objects.filter(request_id=obj.id))
+    def get_votes(self, obj):
+        return len(ToolVote.objects.filter(request_id=obj.id))
