dd5bb2607d51c51532104ccfe3ea2db807ef8787
Eve Ying <eveying96@gmail.com>
2018-11-25 15:52:25 -0500

    use django's builtin signup/signin

--- src/knapsack/knapsack_core/urls.py
+++ src/knapsack/knapsack_core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from knapsack_core import views
+from django.conf.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -7,4 +8,9 @@ urlpatterns = [
     path('request/', views.request_component, name='request'),
     path('library/', views.LibraryView.as_view(), name='library'),
     path('example/', views.single_component, name='single_component'),
+    path('signup', views.SignUp.as_view(), name='signup')
+]
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
