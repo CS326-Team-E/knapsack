5cbe6207baf2c2fb162f931cd5c47e4aca521e03
GitHubdeWill <hd209458inj@gmail.com>
2018-11-27 13:43:12 -0500

    added user specific knapsack

--- src/knapsack/knapsack_core/views.py
+++ src/knapsack/knapsack_core/views.py
@@ -13,43 +13,51 @@ def index(request):
     # Use the welcome.html page for now
     return render(request, 'welcome.html', context={})
 
+
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
+
 class MapView(generic.TemplateView):
     template_name = "map_w_toolbar.html"
 
     def get_context_data(self, **kwargs):
-
         context = super().get_context_data(**kwargs)
-        context['tool_list'] = Tool.objects.all()
-        print(len(Tool.objects.all()))
+        if self.request.user.is_authenticated:
+            userknap = Knapsack.objects.filter(owner__identifier=self.request.user.email).first()
+            print(userknap.tools.all())
+            context['tool_list'] = userknap.tools.all()
+        else:
+            pass
+        # context['tool_list'] = Knapsack.objects.all()
+        # print(len(Tool.objects.all()))
         return context
 
 
 def request_component(request):
-  requests = ToolRequest.objects.all()
-  for tr in requests:
-    tvs = ToolVote.objects.filter(request_id=tr.id)
-    tr.votes = len(tvs)
-    tr.voted = request.user.id in [tv.username.id for tv in tvs]
-    tr.votable = request.user.is_authenticated and not tr.voted
-    tr.button = 'disabled'
-    tr.owner = request.user.id == tr.username.id
-    if tr.voted:
-      tr.button = 'active'
-    elif tr.votable:
-      tr.button = ''
-  requests = sorted( list(requests), key=lambda r:r.votes, reverse=True)
-  return render(request, 'request_component.html', context={
-    'requests': requests
-  })
-  
+    requests = ToolRequest.objects.all()
+    for tr in requests:
+        tvs = ToolVote.objects.filter(request_id=tr.id)
+        tr.votes = len(tvs)
+        tr.voted = request.user.id in [tv.username.id for tv in tvs]
+        tr.votable = request.user.is_authenticated and not tr.voted
+        tr.button = 'disabled'
+        tr.owner = request.user.id == tr.username.id
+        if tr.voted:
+            tr.button = 'active'
+        elif tr.votable:
+            tr.button = ''
+    requests = sorted(list(requests), key=lambda r: r.votes, reverse=True)
+    return render(request, 'request_component.html', context={
+        'requests': requests
+    })
+
+
 class LibraryView(generic.TemplateView):
     template_name = 'library.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['knapsack'] = Knapsack.objects.first()
@@ -60,8 +68,10 @@ class LibraryView(generic.TemplateView):
             context['query'] = ''
         return context
 
+
 def single_component(request):
     return render(request, 'single_component.html', context={})
+
 
 def vote_component(request, request_id):
     tr = get_object_or_404(ToolRequest, id=request_id)
@@ -72,11 +82,14 @@ def vote_component(request, request_id):
         ToolVote.objects.create(username=user, request=tr)
     return redirect('request')
 
+
 def new_component(request):
     print(request.POST)
     user = get_object_or_404(User, id=request.user.id)
-    ToolRequest.objects.create(username=user, title=request.POST.get('title', ''), request=request.POST.get('description', ''))
+    ToolRequest.objects.create(username=user, title=request.POST.get(
+        'title', ''), request=request.POST.get('description', ''))
     return redirect('request')
+
 
 def remove_component(request, request_id):
     tr = get_object_or_404(ToolRequest, id=request_id)
